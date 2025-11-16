736dd0935cb7b57aa7028f7142a4350c3a90b15e
Sagnik2003 <sagnikkayal72@gmail.com>
2025-11-16 16:51:38 +0530

    Joined the P1 and P2 image spreadshhet data

--- preprocess.py
+++ preprocess.py
@@ -117,6 +117,9 @@ class ConvolutionalFeatureExtractor:
 # RUN TEST
 # ----------------------------------------------------------
 if __name__ == "__main__":
-    base_dir = "E:/@IIT_BBS/@Sem 1/ML/Final Project/Safety-Analysis/Dataset/Train_Images"
+    base_dir = "E:/@IIT_BBS/@Sem 1/ML/Final Project/Safety-Analysis/P2"
     extractor = ConvolutionalFeatureExtractor()
+    
+    # batch_features = ConvolutionalFeatureExtractor.batch_CFE(batch_size=20,base_dir=base_dir,greyscale=False, kernel_size=3, target_size=5)
+    # batch_features.shape
     print(extractor)
